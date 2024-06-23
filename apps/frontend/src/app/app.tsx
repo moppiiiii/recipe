005c48cb75738c617c049d704194e2e9a03301b4
moppiiiii <tnktmhssrc@gmail.com>
2024-06-23 11:48:49 +0900

    feat: add router

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,13 +1,20 @@
-import NxWelcome from './nx-welcome';
-
+import HomePage from './pages/home/Home.page';
+import NotFoundPage from './pages/not-found/NotFound.page';
 import './styles/global.scss';
 import './wdyr';
 
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 export function App() {
   return (
-    <div>
-      <NxWelcome title="frontend" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/*" element={<Navigate to="/404" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
